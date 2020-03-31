dcfe8148b30c7cbcb8067ac9ced1124c2621fa19
faqihirfani <47860298+faqihirfani@users.noreply.github.com>
2020-03-31 20:27:45 +0700

    Add files via upload

---
+++ application/views/element/v_footer.php
@@ -0,0 +1,15 @@
+<hr>
+        <div class="footer">
+            <p>PBKK 2020</p>
+        </div>
+
+<script type="text/javascript" src="<?php echo base_url('asset/js/jquery.printPage.js')?>"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $(".btnPrint").printPage();
+    })
+</script>
+
+    </div>
+</body>
+
